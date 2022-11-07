717c8d03fb9365d0d76bb0674bc158608abcdc52
DavidLineman <dline215@gmail.com>
2022-11-07 09:27:44 -0500

    update instaclone with Polymorphic likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+
   def create
     @post = Post.find(params[:post_id])
     current_user.like!(@post)
@@ -8,4 +9,6 @@ class LikesController < ApplicationController
     @post = current_user.posts.find_by(id: params[:post_id])
     current_user.likes.where(post_id: @post.id).delete_all
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :posts, only: [:create, :new] do
-    resources :comments, only: [:create, :new]
-    resources :likes, only: :create do
-      collection do
-        delete :destroy
-      end
-    end
+    resources :comments, only: [:create, :new] do
+      resources :likes, only: [:create, :destroy]
+    end 
   end
+
+  # resources :likes, only: [:create, :destroy]
+
+  # set up custom routes for Posting POST likes and Comment Likes.
+  
   devise_for :users
   root "site#index"
 end
